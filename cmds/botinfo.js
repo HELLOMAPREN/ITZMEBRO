80c246fea20ee7cc78d6ef31e1b6517962275c2e
HELLOMAPREN <45342400+HELLOMAPREN@users.noreply.github.com>
2018-11-26 16:17:37 -0800

    Update botinfo.js

--- cmds/botinfo.js
+++ cmds/botinfo.js
@@ -6,11 +6,11 @@ module.exports.run = async (bot, message, args) => {
  
   let bicon = bot.user.displayAvatarURL
   let embed = new Discord.RichEmbed()
-  .setTitle("<:verified:435160984521408512> | DJ Beatz Information")
-  .addField("❱ Bot Name", "DJ Beatz")
+  .setTitle("MinerDreamsID Information")
+  .addField("❱ Bot Name", "MinersDreamID")
   .setThumbnail(bicon)
   .addField("❱ Bot ID" , `${bot.user.id}`)
-  .addField("❱ Bot Prefix" , "x!")
+  .addField("❱ Bot Prefix" , "d!.")
   .addField("❱ Owner ", "EzraMighty#5576", true)
         .addField("❱ Created On", bot.user.createdAt)
   .addField("❱ Library Bot", " • Discord.js v11.4.2  Nodejs v8.12.0", true)
